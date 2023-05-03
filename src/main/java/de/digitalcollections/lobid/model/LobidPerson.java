678c4469caccf3f0eca28ab7e481263bf40f510f
Christoph Lorenz <christoph.lorenz@bsb-muenchen.de>
2023-05-03 14:10:15 +0200

    Add (first) birth and death date for a person

--- src/main/java/de/digitalcollections/lobid/model/LobidPerson.java
+++ src/main/java/de/digitalcollections/lobid/model/LobidPerson.java
@@ -1,4 +1,32 @@
 package de.digitalcollections.lobid.model;
 
+import java.util.List;
+
 /** type = "DifferentiatedPerson" / "Person" */
-public class LobidPerson extends LobidEntity {}
+public class LobidPerson extends LobidEntity {
+
+  String dateOfBirth;
+  String dateOfDeath;
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(List<String> dateOfBirths) {
+    if (dateOfBirths == null || dateOfBirths.isEmpty()) {
+      return;
+    }
+    this.dateOfBirth = dateOfBirths.get(0);
+  }
+
+  public String getDateOfDeath() {
+    return dateOfDeath;
+  }
+
+  public void setDateOfDeath(List<String> dateOfDeaths) {
+    if (dateOfDeaths == null || dateOfDeaths.isEmpty()) {
+      return;
+    }
+    this.dateOfDeath = dateOfDeaths.get(0);
+  }
+}
