01bcbc65071f29210459ec7199345b09cb0b90a2
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2023-07-24 15:49:41 +0200

    Add depiction to person

--- src/main/java/de/digitalcollections/lobid/model/LobidPerson.java
+++ src/main/java/de/digitalcollections/lobid/model/LobidPerson.java
@@ -7,9 +7,18 @@ public class LobidPerson extends LobidEntity {
 
   String dateOfBirth;
   String dateOfDeath;
+  private List<LobidDepiction> depiction;
 
   public String getDateOfBirth() {
     return dateOfBirth;
+  }
+
+  public String getDateOfDeath() {
+    return dateOfDeath;
+  }
+
+  public List<LobidDepiction> getDepiction() {
+    return depiction;
   }
 
   public void setDateOfBirth(List<String> dateOfBirths) {
@@ -19,14 +28,14 @@ public class LobidPerson extends LobidEntity {
     this.dateOfBirth = dateOfBirths.get(0);
   }
 
-  public String getDateOfDeath() {
-    return dateOfDeath;
-  }
-
   public void setDateOfDeath(List<String> dateOfDeaths) {
     if (dateOfDeaths == null || dateOfDeaths.isEmpty()) {
       return;
     }
     this.dateOfDeath = dateOfDeaths.get(0);
+  }
+
+  public void setDepiction(List<LobidDepiction> depiction) {
+    this.depiction = depiction;
   }
 }
